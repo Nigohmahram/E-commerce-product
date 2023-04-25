864cbb3c645f7612bddb6ed7a4303cbc03bbee71
Nigohmahram <usmonovmaxsudovich@gmail.com>
2023-04-26 02:23:21 +0900

    Add site header shopping cart popup

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-window.addEventListener('DOMContentLoaded', () => {});
+window.addEventListener('DOMContentLoaded', () => {
+     const elSiteHeaderCartLink = document.querySelector('.js-site-header-cart-link'),
+          elSiteHeaderCartModal = document.querySelector('.site-hader__cart-modal');
+
+     if (elSiteHeaderCartLink) {
+          elSiteHeaderCartLink.addEventListener('click', function (evt) {
+               evt.preventDefault();
+
+               elSiteHeaderCartModal.classList.toggle('site-hader__cart-modal--open');
+          });
+     }
+});
